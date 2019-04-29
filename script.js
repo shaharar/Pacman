81bbf49bf60a133b5a0a0fbfb48b17add8670afe
ירדן חלילי <haliliya@auth.ad.bgu.ac.il>
2019-04-29 19:12:03 +0300

    modal dialog

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var start_time;
 var time_elapsed;
 var interval;
 var usersMap = new Map();
-usersMap.set('a','a');
+usersMap.set('a',"a");
 
 Start();
 
@@ -167,8 +167,14 @@ function submit(){
 }
 
 function showWindow(id){
-    hideAllWindows();
+    if (id !== "about"){
+        hideAllWindows();
+    }
+    else{
+        about();
+    }
     $("#"+id).show();
+
 }
 
 function hideAllWindows(){
@@ -193,11 +199,11 @@ function loginValidation(){
     }
 
     else{
-        if (!usersMap.has('username')){
+        if (!usersMap.has(username)){
             alert("Invalid username");
         }
         else{
-            if(!password==usersMap.get('username')){
+            if(password!==usersMap.get(username)){
                 alert("Incorrect password"); 
             }
             else{
@@ -208,4 +214,35 @@ function loginValidation(){
 
     document.getElementById('loginUsername').value="";
     document.getElementById('loginPsw').value="";
+}
+
+
+function about(){
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+$(document).keydown(function(event) { 
+    if (event.keyCode == 27) { 
+        modal.style.display = "none";
+        }
+  });
 }
